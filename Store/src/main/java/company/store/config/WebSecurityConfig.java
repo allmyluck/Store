96c768b1fb03974d4c2cbd288bcfecce704832a0
mozerfucker <tokarevnikita08@gmail.com>
2020-11-05 00:02:30 +0300

    upgrade config

--- Store/src/main/java/company/store/config/WebSecurityConfig.java
+++ Store/src/main/java/company/store/config/WebSecurityConfig.java
@@ -1,15 +1,24 @@
 package company.store.config;
 
+import company.store.services.CustomUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfiguration {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-    /*
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -24,5 +33,4 @@ public class WebSecurityConfig extends WebSecurityConfiguration {
                 .defaultSuccessUrl("/lk").failureUrl("/posts").permitAll()
                 .and().logout().logoutSuccessUrl("/posts").permitAll();
     }
-     */
 }
